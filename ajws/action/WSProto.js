539f5b24dff73d69e14cb010f55a77e3a93a2ac6
SrJohnathan <johnathan.stark42@gmail.com>
2018-04-07 20:16:18 -0300

    Add files via upload

--- ajws/action/WSProto.js
+++ ajws/action/WSProto.js
@@ -24,13 +24,11 @@ class WSProto {
 
         webs.on("connection", function connection(so, req) {
 
+            console.log("connection");
 
-
-
+            console.log("aarays " + Object.keys(obj).length);
 
             so.on('message', function message(message) {
-
-                console.log(message);
 
 
                 var json = JSON.parse(message);
@@ -39,73 +37,128 @@ class WSProto {
                 if (json.method === "open" && json.key === '5895689569855698') {
 
                     if (obj.length > 0) {
-                        for (var i = 0; i < obj.length; i++) {
-                            if (obj[i].id !== json.id) {
-                                obj.push({socket: so, id: json.id});
-                                var datas = {to: json.id, id: "system", value: 'data', status: "ONLI"}
-                                if ('locahostAd' === json.id) {
-                                    soad = so;
-                                    var data = {to: json.id, id: "system", value: 'data', status: "ONLI", precesse: obj.length, method: 'open'};
-                                    so.send(JSON.stringify(data));
-                                } else {
-                                    if (soad !== null) {
-                                        soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
-                                    }
-                                    so.send(JSON.stringify(datas));
-                                }
+                        var existe = false;
+                        var obtmp = null
+
+                        obj.forEach(function (ob) {
+                            if (ob.id === json.id) {
+                                obtmp = ob;
+                                existe = true;
+                            }
+                        });
+
+                        if (existe === false) {
+
+                            console.log("add :" + json.id);
+                            obj.push({socket: so, id: json.id});
+
+                            var datas = {to: json.id, id: "system", value: 'data', status: "ONLI"}
+
+
+                            if ('locahostAd' === json.id) {
+
+                                soad = so;
+
+                                var data = {to: json.id, id: "system", value: 'data', status: "ONLI", precesse: obj.length, method: 'open'};
+
+                                so.send(JSON.stringify(data));
+
 
                             } else {
 
-                                for (var i = 0; i < obj.length; i++) {
+                                if (soad !== null) {
 
-                                    if (obj[i].id === json.id) {
-
-                                        console.log("novamente :" + json.id);
-                                        obj.splice(i, 1, {socket: so, id: json.id});
-                                        console.log(obj);
-                                    }
+                                    soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
                                 }
 
-                                var datas = {to: json.id, id: "system", value: 'data', status: "ONLI"}
-                                if ('locahostAd' === json.id) {
-                                    soad = so;
-                                    var data = {to: json.id, id: "system", value: 'data', status: "ONLI", precesse: obj.length, method: 'open'};
-                                    so.send(JSON.stringify(data));
-                                } else {
-                                    if (soad !== null) {
-                                        soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
-                                    }
-                                    so.send(JSON.stringify(datas));
-                                }
-
+                                so.send(JSON.stringify(datas));
                             }
-                        }
-                    } else {
-                        obj.push({socket: so, id: json.id});
-                        var datas = {to: json.id, id: "system", value: data, status: "ONLI"}
-                        if ('locahostAd' === json.id) {
-                            soad = so;
-                            var datas = {to: json.id, id: "system", value: data, status: "ONLI", precesse: obj.length, method: 'open'};
-                            so.send(JSON.stringify(datas));
+
+
+
+
                         } else {
-                            if (soad !== null) {
-                                soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
 
+                            console.log("novamente :" + obtmp.id + "nn "+ obj.indexOf(obtmp));
+
+                            obj.splice(obj.indexOf(obtmp), 1, {socket: so, id: json.id});
+
+                            console.log("aarays " + Object.keys(obj).length);
+
+                            var datas = {to: json.id, id: "system", value: 'data', status: "ONLI"};
+
+                            if ('locahostAd' === json.id) {
+
+                                soad = so;
+
+                                var data = {to: json.id, id: "system", value: 'data', status: "ONLI", precesse: obj.length, method: 'open'};
+
+
+                                so.send(JSON.stringify(data));
+
+                            } else {
+
+                                if (soad !== null) {
+
+                                    soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
+                                }
+                                so.send(JSON.stringify(datas));
                             }
+
+
+
+                        }
+
+
+
+
+
+                    } else {
+
+
+                        obj.push({socket: so, id: json.id});
+
+                        var datas = {to: json.id, id: "system", value: 'data', status: "ONLI"};
+
+
+                        if ('locahostAd' === json.id) {
+
+                            soad = so;
+
+                            var data = {to: json.id, id: "system", value: 'data', status: "ONLI", precesse: obj.length, method: 'open'};
+
+                            so.send(JSON.stringify(data));
+
+
+                        } else {
+
+                            if (soad !== null) {
+
+                                soad.send(JSON.stringify({to: 'locahostAd', id: "system", method: 'ADD', idadd: json.id, precesse: obj.length}));
+                            }
+
                             so.send(JSON.stringify(datas));
                         }
                     }
 
+
+
                 }
+
+
 //{"data":{"barcode":"014002","fun":"CONSULTA"},"fn":"VF","id":"b71e9632-5bed-4ce2-bea6-d976e6678e4b","key":"5895689569855698","method":"men","plataform":"MOB","status":"ONLI","to":"b71e9632-5bed-4ce2-bea6-d976e7678e4b"}
+
+
+
+
                 if (json.method === "men" && json.key === '5895689569855698') {
 
                     var to = json.to;
                     var id = json.id;
-                    var barcode = json.data.barcode;
+
                     var fun = json.data.fun;
 
-                    console.log(to);
+
 
                     for (var i = 0; i < obj.length; i++) {
                         var sok = obj[i].socket;
@@ -137,13 +190,10 @@ class WSProto {
 
             });
 
-
-
-
-
             so.on('error', function err(err) {
 
-
+                console.log(so.readyState);
+                console.log(err);
                 webs.clients.forEach(function each(client) {
                     if (client === so) {
 
@@ -184,6 +234,29 @@ class WSProto {
 
     }
 
+    intervall() {
+
+        const interval = setInterval(function ping() {
+            webs.clients.forEach(function each(ws) {
+                if (ws.isAlive === false)
+                    return ws.terminate();
+
+                ws.isAlive = false;
+                ws.ping(noop);
+            });
+        }, 30000);
+
+    }
+
+}
+
+
+
+
+function noop() {}
+
+function heartbeat() {
+    this.isAlive = true;
 }
 
 module.exports = WSProto
